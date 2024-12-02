6a86836c5bd0d880532ec81cfb05d75d598f1143
AnnikaRings <annika.rings@optellum.com>
2024-12-02 09:31:02 +0000

    some Day 2 formatting

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,35 +1,43 @@
-module Day02 
-    (day2asol,
-    readInp)
-    where 
-import Data.List(tails, init)
+module Day02
+  ( day2asol,
+    readInp,
+  )
+where
+
+import Data.List (init, tails)
 
 readInt :: String -> Int
 readInt = read
 
 -- | read Day2 input
-readInp fname = do 
-    contents <- readFile fname 
-    let reports = lines contents  
-    let levels = map (map readInt . words) reports
-    return levels
--- Part 1 
+readInp :: FilePath -> IO [[Int]]
+readInp fname = do
+  contents <- readFile fname
+  let reports = lines contents
+  let levels = map (map readInt . words) reports
+  return levels
+
+-- Part 1
+isMonotonic :: Ord b => [b] -> Bool
+
 -- | checks if list is monotonic
-isMonotonic list = 
-    isMonotonicIncreasing list 
+isMonotonic list =
+  isMonotonicIncreasing list
     || isMonotonicDecreasing list
+  where
+    isMonotonicIncreasing [x] = True
+    isMonotonicIncreasing l = and $ zipWith (<) l (map head $ tail $ init $ tails l)
+    isMonotonicDecreasing [x] = True
+    isMonotonicDecreasing l = and $ zipWith (>) l (map head $ tail $ init $ tails l)
 
-    where
-        isMonotonicIncreasing [x] = True 
-        isMonotonicIncreasing l = and $ zipWith (<) l (map head $ tail $ init$ tails l)
-        isMonotonicDecreasing [x] = True
-        isMonotonicDecreasing l = and $ zipWith (>) l (map head $ tail $ init$ tails l)
+diffBelow4 :: (Ord a, Num a) => [a] -> Bool
+diffBelow4 l = and $ map ((<= 3) . abs) $zipWith (-) l (map head $ tail $ init $ tails l)
 
-diffBelow4 l = and $ map ((<=3) .abs) $zipWith (-) l (map head $ tail $ init$ tails l)
 -- | filter reports that ar monotonic
-day2asol = do 
-    reports <- readInp "input/day02.txt"
-    print(length reports)
-    print (filter (\x -> isMonotonic x&& diffBelow4 x)reports)
-    let sol = (length .filter (\x -> isMonotonic x&& diffBelow4 x)) reports 
-    print sol
+day2asol :: IO ()
+day2asol = do
+  reports <- readInp "input/day02.txt"
+  print (length reports)
+  print (filter (\x -> isMonotonic x && diffBelow4 x) reports)
+  let sol = (length . filter (\x -> isMonotonic x && diffBelow4 x)) reports
+  print sol
