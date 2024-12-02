d05ff42a3097145a1db3fd95caa3876ddaaba3ce
AnnikaRings <annika.rings@optellum.com>
2024-12-02 17:54:10 +0000

    added Day 2 Part 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,6 @@
 module Day02
   ( day2asol,
-    readInp,
+    day2bsol,
   )
 where
 
@@ -25,22 +25,62 @@ isMonotonic list =
   isMonotonicIncreasing list
     || isMonotonicDecreasing list
   where
-    isMonotonicIncreasing l = and $ zipWith (<) l (map head $ tail $ init $ tails l)
-    isMonotonicDecreasing l = and $ zipWith (>) l (map head $ tail $ init $ tails l)
+    isMonotonicIncreasing l =
+      and $
+        zipWith (<) l (map head $ tail $ init $ tails l)
+    isMonotonicDecreasing l =
+      and $
+        zipWith (>) l (map head $ tail $ init $ tails l)
 
 diffBelow4 :: (Ord a, Num a) => [a] -> Bool
 diffBelow4 l = all ((<= 3) . abs) $zipWith (-) l (map head $ tail $ init $ tails l)
 
--- | filter reports that ar monotonic
+safeReps :: (Ord a, Num a) => [[a]] -> [[a]]
+safeReps =
+  filter
+    ( \x ->
+        isMonotonic x && diffBelow4 x
+    )
+
+numSafeReps :: [[Int]] -> Int
+numSafeReps = length . safeReps
+
+-- | filter reports that are safe
 day2asol :: IO ()
 day2asol = do
   reports <- readInp "input/day02.txt"
+  let sol = numSafeReps reports
+  print sol
+
+-- Part 2
+unsafeReports :: (Ord a, Num a) => [[a]] -> [[a]]
+unsafeReports =
+  filter
+    ( not
+        . ( \x ->
+              isMonotonic x && diffBelow4 x
+          )
+    )
+
+isSafeWithOneRemoved :: (Ord a, Num a) => [a] -> Bool
+isSafeWithOneRemoved l = go l (length l)
+  where
+    go _ 0 = False
+    go ls n =
+      isMonotonic (take (n -1) ls ++ drop n ls)
+        && diffBelow4 (take (n -1) ls ++ drop n ls)
+        || go ls (n -1)
+
+day2bsol :: IO ()
+day2bsol = do
+  reports <- readInp "input/day02.txt"
+
+  let unsaveReps = unsafeReports reports
   let sol =
-        ( length
-            . filter
-              ( \x ->
-                  isMonotonic x && diffBelow4 x
-              )
-        )
-          reports
+        numSafeReps reports
+          + ( length
+                . filter
+                  isSafeWithOneRemoved
+            )
+            unsaveReps
   print sol
