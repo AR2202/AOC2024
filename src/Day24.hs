b4746069b7474770f212f279a4737a8f55cffb1a
AnnikaRings <annika.rings@optellum.com>
2025-01-11 21:48:01 +0000

    added Day 24 Part 2

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,4 +1,4 @@
-module Day24 (testParseLineOp, day24ex) where
+module Day24 (testParseLineOp, day24ex, day24b, toBinary, findInday24bMap) where
 
 import Data.Foldable (Foldable (foldl'))
 import Data.List (isPrefixOf, sort)
@@ -101,6 +101,9 @@ toDecimal _ [] = 0
 toDecimal n (x : xs) =
   x * (2 ^ n)
     + toDecimal (n + 1) xs
+toBinary :: Integral a => a -> [a]
+toBinary 0 = []
+toBinary n = n `mod` 2 : toBinary (n `div` 2)
 
 day24ex :: IO ()
 day24ex = do
@@ -113,3 +116,65 @@ day24ex = do
     toDecimal 0 $
       map bool2Num $
         evalAllZs m
+
+-- PArt 2 
+
+evalAll :: [Char] -> M.Map String WireMap -> [Bool]
+evalAll c m = map (evalWireMap m) zkeys
+  where
+    zkeys =
+      sort $
+        filter (isPrefixOf c) $
+          M.keys m
+
+evalAllAsBinary :: Num b => [Char] -> M.Map String WireMap -> [b]
+evalAllAsBinary c m = map bool2Num $ evalAll c m 
+
+evalAllAsDecimal c m = toDecimal 0 $ evalAllAsBinary c m 
+
+differences m = abs (allZs - (allYs + allXs))
+    where allZs = evalAllAsDecimal "z" m 
+          allYs = evalAllAsDecimal "y" m 
+          allXs = evalAllAsDecimal "x" m 
+
+day24b :: IO ()
+day24b = do
+  contents <- readFile "input/day24.txt"
+  let splitOnBlank = splitOn "\n\n" contents
+  let literals = lines $ head splitOnBlank
+  let ops = lines $ last splitOnBlank
+  let m = makeWireMap literals ops
+  print m 
+  print $M.lookup "z11" m 
+  print $M.lookup "z31" m 
+  print $M.lookup "z38" m 
+  print $M.lookup "z45" m 
+  print $ toBinary $ differences m 
+
+--11, 31, 38, 45
+
+findInday24bMap :: [Char] -> IO ()
+findInday24bMap s = do
+  contents <- readFile "input/day24.txt"
+  let splitOnBlank = splitOn "\n\n" contents
+  let literals = lines $ head splitOnBlank
+  let ops = lines $ last splitOnBlank
+  let m = makeWireMap literals ops
+  print $ M.lookup s m 
+  
+--z31, dmh, z38,dvq, rpv, z11, rpb, ctg
+-- ctg,dmh,dvq,rpb,rpv,z11,z31,z38
+
+--z31 And x31 y31
+--ctw: XOR x31 y31
+--bvk: And x38 y38 
+--z38 bvk or trm 
+--bvk to hhv
+--"dmh",Xor "fgs" "ctw"
+--"bmm",And "fgs" "ctw"
+--"dtf",Or "dmh" "bmm"
+--"z11",And "dvh" "hnn"
+--"rpv",Xor "hnn" "dvh"
+--"dvq",Xor "hhv" "pqr"
+--"ctg",And "y15" "x15"
+--"rpb",Xor "x15" "y15"
